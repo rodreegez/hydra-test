2e5579b6dd6fc573aeb9208bbc2036c079f25e59
Adam <electronicbattleweapon7@gmail.com>
2010-12-14 16:29:05 +0000

    add dumy tests to make things SLOW

---
+++ test/unit/3_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DummyTest3 < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "dummy test" do
+    sleep 10
+    assert true
+  end
+end
---
+++ test/unit/6_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DummyTest6 < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "dummy test" do
+    sleep 10
+    assert true
+  end
+end
